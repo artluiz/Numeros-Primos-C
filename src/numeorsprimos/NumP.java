486526e3e664ffa2e3f7458c9345fab49fa34b11
Luiz Arthur <luizarthursm@gmail.com>
2019-11-06 11:34:56 -0200

    Versão 1.0

---
+++ src/numeorsprimos/NumP.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package numeorsprimos;
+
+/**
+ *
+ * @author Luiz Arthur
+ */
+public class NumP {
+    
+    private int n;
+    private boolean np = false;
+
+    public NumP(int n) {
+        this.n = n;
+    }
+
+    public int getN() {
+        return n;
+    }
+
+    public void setN(int n) {
+        this.n = n;
+    }
+
+    public boolean isNp() {
+        return np;
+    }
+
+    public void setNp(boolean np) {
+        this.np = np;
+    }
+    
+    
+}
---
+++ src/numeorsprimos/NumeorsPrimos.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package numeorsprimos;
+
+/**
+ *
+ * @author Luiz Arthur
+ */
+public class NumeorsPrimos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        int m=98;
+        NumP vet[] = new NumP[m];
+        
+        for(int l=0;l<m;l++){
+            vet[l] = new NumP(0);
+        }
+        
+        for(int l=0;l<m;l++){
+            System.out.println(vet[l].getN());
+        }
+        int k=0;
+        for(int i=2;i<100; i++){
+            vet[k].setN(i);
+            k++;
+        }
+        
+        for(int i=2;i<m; i++){
+            for (int j=2;j<vet[i].getN(); j++) {
+                if (vet[i].getN()%j != 0){
+                    vet[i].setNp(true);
+                }
+            }
+        }
+        
+        for (int l=0;l<m;l++){
+            if(vet[l].isNp()){
+                System.out.println("O número "+vet[l].getN()+" é primo");
+            }
+        }
+    }
+    
+}
